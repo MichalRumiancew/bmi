eecedfae9322027037b6c9892a75edba7273543e
Michal <michalr11@gmail.com>
2019-03-24 14:08:46 +0100

    1

---
+++ src/main/java/calculate/BMICalculator.java
@@ -0,0 +1,25 @@
+package calculate;
+
+public class BMICalculator implements Calculator {
+
+    private int weight;
+    private int height;
+
+    public BMICalculator(int height, int wwight) {
+
+
+    }
+
+    public String calculate() {
+
+        return "";
+    }
+
+    public String interpret() {
+
+        return "  ";
+    }
+
+}
+
+//
